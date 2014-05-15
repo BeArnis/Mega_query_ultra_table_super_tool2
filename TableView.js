b9ccb64dd0e2e2f6a0b563ab6ebdf8f4145bbbc8
BeArnis <berzins555@gmail.com>
2014-05-15 10:02:21 +0300

    got plugins for column form to not trow errors

--- TableView.js
+++ TableView.js
@@ -15,64 +15,132 @@ TableView = function() {
 
     var gridContainer;
 
+    var columns;
+
     function init(init_div, init_node, init_graph) {
         div = init_div;
         node = init_node;
         graph = init_graph;
         var data = [];
         var columns = [];
-        //console.log(div);
+
+        // make the container where slickgrid will go inot
         gridContainer = d3.select(div).append('div') // make in table selection
         .classed('query-result-redering-container', true)
             .style('left', '30px')
             .style('background-color', 'white');
 
+        // make slickgrid
         table_grid = new Slick.Grid(gridContainer.node(), data, columns, options);
 
+        // // asigns menu to a value
+        // var headerMenuPlugin = new Slick.Plugins.HeaderMenu({});
+
+        // headerMenuPlugin.onBeforeMenuShow.subscribe(function(e, args) {
+        //   var menu = args;
+        //   console.log(e, args)
+        //   // We can add or modify the menu here, or cancel it by returning false.
+        //   var i = menu.items.length;
+        //   menu.items.push({
+        //     title: "Menu item " + i,
+        //     command: "item" + i
+        //   });
+        // });
+
+        // headerMenuPlugin.onCommand.subscribe(function(e, args) {
+        //   alert("Command: " + args.command);
+        // });
+
+        // table_grid.registerPlugin(headerMenuPlugin);
+
+
+        // change the sort of a column
         table_grid.onHeaderClick.subscribe(function(e, args) {
             var columnID = args.column.name;
+            console.log(args.column.name);
 
-            var column_change_index = _.chain(node.columns)
-                .map(function(column) {
-                    return column.column_label;
-                })
-                .indexOf(args.column.name)
-                .value();
+            console.log(e.srcElement);
+            $(e.srcElement).popover({
+                placement: 'bottom', //placement of the popover. also can use top, bottom, left or     right
+                html: 'false', //needed to show html of course
+                content: 'abc'// hope this should b
+            })
+
+            $(e.srcElement).popover('show');
+            // if click is on the make column header we make a column and dont change the sort
+            if (args.column.name == 'ss') {
+                console.log(args, e.srcElement, this)
+
+                d3.select(e.srcElement)
+                    .attr('data-toggle', 'popover')
+                    .attr('data-placement', 'top')
+                    .attr('data-content', 'Vivamus sagittis lacus')
 
 
-            if (node.columns[column_change_index].sort == 'ascending') {
-                node.columns[column_change_index].sort = 'descending';
-            } else if (node.columns[column_change_index].sort == 'descending') {
-                node.columns[column_change_index].sort = 'none';
+
+            } else if (args.column.name == 'add_column') {
+                console.log('make col')
+
+                node.columns.push({
+                    // data property
+                    'id': guid(),
+                    'type': 'aggregate',
+                    'column_label': 'ss',
+                    'aggregation_function': 'count',
+                    'what_to_aggregate': 'l1',
+                    'sort': 'descending',
+                    'id': '123',
+                    'width': 330
+                });
+
+                _.defer(function() {
+                    render_graph(graph);
+                    fill_tables(graph);
+                });
+
             } else {
-                node.columns[column_change_index].sort = 'ascending';
+                // gets all column lables
+                var column_change_index = _.chain(node.columns)
+                    .map(function(column) {
+                        return column.column_label;
+                    })
+                    .indexOf(args.column.name)
+                    .value();
+
+                // toggles sort
+                if (node.columns[column_change_index].sort == 'ascending') {
+                    node.columns[column_change_index].sort = 'descending';
+                } else if (node.columns[column_change_index].sort == 'descending') {
+                    node.columns[column_change_index].sort = 'none';
+                } else {
+                    node.columns[column_change_index].sort = 'ascending';
+                }
+
+                //render_graph(graph);
+                _.defer(function() {
+                    render_graph(graph);
+                    fill_tables(graph);
+                });
+
+                console.log('sort');
+                return;                
             }
 
-            //render_graph(graph);
-            _.defer(function() {
-                render_graph(graph);
-                fill_tables(graph);
-            });
-
-            //console.log(node.columns[column_change_index]);
-            console.log('sort');
-            return;
         });
 
         table_grid.setSelectionModel(new Slick.RowSelectionModel({
             selectActiveRow: true
         }));
 
+        // selection handler
         table_grid.onSelectedRowsChanged.subscribe(function() {
             var selected_row_indexes = table_grid.getSelectedRows();
 
             var all_table_row_values = _.map(table_grid.getData(), function(obj) {
-
                 return obj;
             })
 
-            var current_selected_values = _.map(selected_row_indexes, function(index) { // works, gets an object\
-
+            var current_selected_values = _.map(selected_row_indexes, function(index) { 
                 return all_table_row_values[index][node.name];
             })
             var arr = [];
@@ -87,28 +155,25 @@ TableView = function() {
             }
         });
 
+        // resize column function
         table_grid.onColumnsResized.subscribe(function(e, args)
         { 
-            var cols = table_grid.getColumns(); // get all grid columns
-            // can get all widths 
-            // indexes are ok I guess or not
-            // need node
+            var cols = table_grid.getColumns(); // get all grid columns 
             _.each(node.columns, function(column, i) {
                 column.width = cols[i + 1].width;
             })
             console.log(cols, node.columns[0])
         }) 
-        // table_grid.onColumnsResized = function() {
-        //     alert("Resized");
-        // }
+
 
         refresh_view_from_node();
         return that;
     }
 
 
+    // puts rigth data into cells
+    function render_cell_data(row, cell, value, columnDef, dataContext) { 
 
-    function render_cell_data(row, cell, value, columnDef, dataContext) { // selection is an array
 
         var data_value = value && value.value || '';
         var matching_prefix_def = _(graph.prefixes).find(function(prefix_def) {
@@ -137,7 +202,7 @@ TableView = function() {
                 return node.geometry.height - 80 + 'px';
             });
 
-        var columns = _.map(node.columns, function(column) {
+        columns = _.map(node.columns, function(column) {
             return {
                 id: column_name_generator(graph, node, column),
                 name: column.column_label,
@@ -148,7 +213,7 @@ TableView = function() {
             };
         });
 
-
+        // adds first column
         columns.unshift({
             id: node.name,
             name: node.name,
@@ -156,6 +221,21 @@ TableView = function() {
             formatter: render_cell_data,
             width: 330
         })
+
+        columns.push({
+            id: 'add_column',
+            name: 'add_column',
+            field: 'add_column',
+            formatter: render_cell_data,
+            sortable: 'none',
+            width: 330
+        })
+
+        d3.selectAll('#add_column')
+            .on('click', function(d) {
+                console.log('make_column', d);
+            })
+
 
         table_grid.setColumns(columns);
 
